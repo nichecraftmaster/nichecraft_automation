141ce97b63518e1dd3415c18ed5004124273db31
nichecraftmaster <nichecraftmaster@gmail.com>
2024-10-08 19:07:44 -0400

    Update generate-report.js

--- netlify/functions/generate-report.js
+++ netlify/functions/generate-report.js
@@ -1,1 +1,11 @@
-
+// /netlify/functions/generate-report.js
+exports.handler = async (event, context) => {
+  const name = event.queryStringParameters.name || 'User';
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello, ${name}! Your report has been generated.`,
+    }),
+  };
+};
